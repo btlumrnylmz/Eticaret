b87d624fe830e5436ea93c4241c23a5b67b88e75
btlumrnylmz <betulumran.6565@gmail.com>
2025-04-27 12:27:16 +0300

    Add project files.

---
+++ Eticaret.Data/Configurations/SlideConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Eticaret.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Eticaret.Data.Configurations
+{
+    internal class SlideConfiguration : IEntityTypeConfiguration<Slide>
+    {
+        public void Configure(EntityTypeBuilder<Slide> builder)
+        {
+            builder.Property(x => x.Title).HasMaxLength(250);
+            builder.Property(x => x.Description).HasMaxLength(250);
+            builder.Property(x => x.Link).HasMaxLength(100);
+            builder.Property(x => x.Image).HasMaxLength(100);
+            
+        }
+    }
+}
