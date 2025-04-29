a96d8614163fadb724d56ad507a6bb110b516e65
btlumrnylmz <betulumran.6565@gmail.com>
2025-04-29 16:41:39 +0300

    Yeni Özellikler ekledi

--- Eticaret.Core/Entities/AppUser.cs
+++ Eticaret.Core/Entities/AppUser.cs
@@ -1,23 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Eticaret.Core.Entities
 {
     public class AppUser : IEntity
     {
         public int Id { get; set; }
+        [Display(Name="Adı")]
         public string Name { get; set; }
+        [Display(Name = "Soyadı")]
         public string Surname { get; set; }
         public string Email { get; set; }
+        [Display(Name = "Telefon")]
         public string? Phone { get; set; }
+        [Display(Name = "Email")]
         public string Password { get; set; }
+        [Display(Name = "Kullanıcı Adı")]
         public string? UserName { get; set; }
+        [Display(Name = "Aktif?")]
         public bool IsActive { get; set; }
+        [Display(Name = "Admin?")]
         public bool IsAdmin { get; set; }
+        [Display(Name = "Kayıt Tarihi"),ScaffoldColumn(false)]
         public DateTime CreateDate { get; set; }= DateTime.Now;
+        [ScaffoldColumn(false)]
         public Guid? UserGuid { get; set; }= Guid.NewGuid();
     }
 }
--- Eticaret.Core/Entities/Brand.cs
+++ Eticaret.Core/Entities/Brand.cs
@@ -1,15 +1,22 @@
-﻿namespace Eticaret.Core.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eticaret.Core.Entities
 {
     public class Brand:IEntity
     {
         public int Id { get; set; }
+        [Display(Name = "Adı")]
         public string Name { get; set; }
+        [Display(Name = "Açıklama")]
         public string? Description { get; set; }
         public string? Logo { get; set; }
+        [Display(Name = "Aktif")]
         public bool IsActive { get; set; }
-       
+        [Display(Name = "Sıra No")]
+
         public int OrderNo { get; set; }
-        public DateTime CreateDate { get; set; }
+        public DateTime? CreateDate { get; set; }
+        [Display(Name = "Kayıt Tarihi"), ScaffoldColumn(false)]
         public IList<Product> Products { get; set; }
 
     }
